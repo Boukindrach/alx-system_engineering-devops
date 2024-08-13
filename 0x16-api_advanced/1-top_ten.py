e389e1383473fcc1bcfb0163d2772efc15b888f3
Rachid Boukind <boukind.r1@@gmail.com>
2024-08-13 18:22:33 +0000

    a

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,29 +3,28 @@
 """
 prints the titles of the first 10 hot posts listed for a given subreddit.
 """
-from requests import get
 
+import requests
 
 def top_ten(subreddit):
     """
-    ****
+    Prints the titles of the top 10 hot posts for a given subreddit.
+    If the subreddit is invalid or an error occurs, prints "None".
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
+    if not isinstance(subreddit, str):
         print("None")
+        return
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
 
     try:
-        my_data = results.get('data').get('children')
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
 
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except (requests.RequestException, KeyError, ValueError):
         print("None")
