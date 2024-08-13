d295a30bf93aa51928d2d4ff1200f844b02c9f62
Rachid Boukind <boukind.r1@@gmail.com>
2024-08-13 18:06:59 +0000

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 
 """gives number of subscribers"""
-import requests
+from requests import get
 
 def number_of_subscribers(subreddit):
     """"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "Custom User Agent 1.0"
-    }
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        return results.get('data').get('subscribers')
 
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-
-    except Exception as e:
+    except Exception:
         return 0
